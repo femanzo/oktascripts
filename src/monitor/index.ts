4123c77a55fe1317002d80a20496d830a4b4ed59
Fernando Manzo <fmanzo@tevora.com>
2024-02-19 21:18:17 -0300

    Added a new error and some other stuff

--- src/monitor/index.ts
+++ src/monitor/index.ts
@@ -5,13 +5,17 @@
 import * as Sentry from "@sentry/bun";
 
 Sentry.init({
-    dsn: "https://107b491825f1ccdda4843b6aba2872ac@o4506776027004928.ingest.sentry.io/4506776028446720",
+    dsn: process.env.SENTRY_DSN,
     // Performance Monitoring
     tracesSampleRate: 1.0, // Capture 100% of the transactions
 });
 
-try {
-    throw new Error('Sentry Bun test');
-} catch (e) {
-    Sentry.captureException(e);
+main()
+
+function main() {
+    try {
+        throw new Error('This is a new error');
+    } catch (e) {
+        Sentry.captureException(e);
+    }
 }
